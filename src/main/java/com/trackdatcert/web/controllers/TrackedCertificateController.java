3aba6a7aa7c367df953820ee8707187b0178523a
julleb <julius.bladh@gmail.com>
2025-03-23 17:27:26 +0100

    Controller: deleteCert added

--- src/main/java/com/trackdatcert/web/controllers/TrackedCertificateController.java
+++ src/main/java/com/trackdatcert/web/controllers/TrackedCertificateController.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -42,7 +43,8 @@ public class TrackedCertificateController {
     }
 
     @DeleteMapping("/{name}")
-    public ResponseEntity<String> deleteTrackedCertificate() {
+    public ResponseEntity<String> deleteTrackedCertificate(@PathVariable(value = "name") String name) {
+        trackedCertificateService.deleteTrackedCertificate(name);
         return ResponseEntity.noContent().build();
     }
 }
